24d0e9fd7b340cd444c2a1a8be93e008b8f65b55
Léo Mora <l.mora@outlook.fr>
2021-09-13 14:00:04 +0200

    Code cleaning (conf files)

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -1,14 +1,12 @@
-const paths = require('./paths')
-
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
-const CopyWebpackPlugin = require('copy-webpack-plugin')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin') // extract css to files
-const autoprefixer = require('autoprefixer')
+const paths = require('./paths');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const autoprefixer = require('autoprefixer');
 
 module.exports = {
 
-  // Shorthand to module inclusion
   resolve: {
     alias: {
       components: paths.src + '/components',
@@ -18,19 +16,15 @@ module.exports = {
     }
   },
 
-  // Where webpack looks to start building the bundle
   entry: [paths.src + '/index.js'],
 
-  // Where webpack outputs the assets and bundles
   output: {
     path: paths.build,
     filename: '[name].bundle.js',
     publicPath: '/',
   },
 
-  // Customize the webpack build process
   plugins: [
-    // Removes/cleans build folders and unused assets when rebuilding
     new CleanWebpackPlugin(),
 
     new MiniCssExtractPlugin({
@@ -38,7 +32,6 @@ module.exports = {
       chunkFilename: '[id].[contenthash].css',
     }),
 
-    // Copies files from target to destination folder
     new CopyWebpackPlugin({
       patterns: [
         {
@@ -51,22 +44,17 @@ module.exports = {
       ],
     }),
 
-    // Generates an HTML file from a template
-    // Generates deprecation warning: https://github.com/jantimon/html-webpack-plugin/issues/1501
     new HtmlWebpackPlugin({
       favicon: paths.res + '/images/icons/favicon.ico',
-      template: paths.public + '/index.html', // template file
-      filename: 'index.html', // output file
+      template: paths.public + '/index.html',
+      filename: 'index.html',
     }),
   ],
 
-  // Determine how modules within the project are treated
   module: {
     rules: [
-      // JavaScript: Use Babel to transpile JavaScript files
       { test: /\.(js|jsx)$/, exclude: /node_modules/, use: ['babel-loader'] },
 
-      // Styles: Inject CSS into the head with source maps
       {
         test: /\.(css|scss|sass)$/,
         use: [
--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -4,13 +4,10 @@ const common = require('./webpack.common.js')
 const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin')
 
 module.exports = merge(common, {
-  // Set the mode to development or production
   mode: 'development',
 
-  // Control how source maps are generated
   devtool: 'inline-source-map',
 
-  // Spin up a server for quick development
   devServer: {
     historyApiFallback: true,
     static: paths.build,
@@ -22,18 +19,14 @@ module.exports = merge(common, {
 
   module: {
     rules: [
-      // ... other rules
       {
         test: /\.[js]sx?$/,
         exclude: /node_modules/,
         use: [
-          // ... other loaders
           {
             loader: require.resolve('babel-loader'),
             options: {
-              // ... other options
               plugins: [
-                // ... other plugins
                 require.resolve('react-refresh/babel'),
               ].filter(Boolean),
             },
@@ -43,7 +36,6 @@ module.exports = merge(common, {
     ],
   },
   plugins: [
-    // new webpack.HotModuleReplacementPlugin(),
     new ReactRefreshWebpackPlugin({ overlay: false }),
   ].filter(Boolean),
 })
