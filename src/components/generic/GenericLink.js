38b6c663c73db53065b2d6f621c647cc345892ee
Léo Mora <l.mora@outlook.fr>
2021-09-20 14:21:14 +0200

    Secure "mailto:" url

--- src/components/generic/GenericLink.js
+++ src/components/generic/GenericLink.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
+import Mailto from 'react-mailto.js';
 
 class GenericLink extends Component {
 	static propTypes = {
@@ -14,11 +15,23 @@ class GenericLink extends Component {
 
 	generateLink() {
 		const url = this.props.to;
-		console.error(this.props)
 		if (url.startsWith("/")) {
-			return <Link to={url} {...this.props}>{this.props.children}</Link>
+			return <Link to={url} {...this.props}>{this.props.children}</Link>;
+		} else if (url.startsWith("mailto:")) {
+			const email = url.split("mailto:")[1];
+			console.error(email)
+			return <Mailto
+				secure={true}
+				to={email}
+				subject="Tchap contact"
+				rel="noreferrer noopener nofollow"
+				target="_blank"
+				{...this.props}>
+				{this.props.children}
+			</Mailto>;
+			//return <a href={url} target="_blank" rel="noreferrer noopener nofollow" {...this.props}>{this.props.children}</a>
 		} else {
-			return <a href={url} target="_blank" rel="noreferrer noopener nofollow" {...this.props}>{this.props.children}</a>
+			return <a href={url} target="_blank" rel="noreferrer noopener nofollow" {...this.props}>{this.props.children}</a>;
 		}
 	}
 
