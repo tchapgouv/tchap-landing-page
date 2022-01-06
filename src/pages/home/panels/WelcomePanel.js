a75a30c2ee27251c6bee9d9bad9a15c47a7f254d
Estelle Comment <estelle.comment@gmail.com>
2022-01-06 14:34:38 +0100

    Update description of Tchap

--- src/pages/home/panels/WelcomePanel.js
+++ src/pages/home/panels/WelcomePanel.js
@@ -13,7 +13,7 @@ class WelcomePanel extends Component {
 					<Container maxWidth="lg">
 						<Grid container spacing={3}>
 							<Grid item md={7} xs={12} className="tc_WelcomePanel_block">
-								<div className="tc_WelcomePanel_title">Tchap, la messagerie instantanée de confiance de l'Administration</div>
+								<div className="tc_WelcomePanel_title">Tchap, c'est mieux que Slack</div>
 								<div className="tc_WelcomePanel_content">Conçue pour les agents des trois fonctions publiques, pour communiquer facilement en toute sécurité.</div>
 								<div className="tc_WelcomePanel_subcontent">Utilisée par plus de 260 000 agents publics.</div>
 								<div className="tc_WelcomePanel_content">
