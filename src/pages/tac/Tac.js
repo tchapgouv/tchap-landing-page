fcca72d8e5f07a21f46ac3a12f6041fa036148e7
Estelle Comment <estelle.comment@gmail.com>
2022-01-11 11:40:35 +0100

    Example text in CGU

--- src/pages/tac/Tac.js
+++ src/pages/tac/Tac.js
@@ -5,7 +5,41 @@ class Tac extends Component {
 	render() {
 		return (
 			<div className="tc_Tac">
-        coucou
+        <h1>Conditions générales d'utilisation</h1>
+
+        <div className="tc_Tac_paragraph">
+          <h2>Ceci est un gros titre, pour une grande partie</h2>
+          <div className="tc_Tac_subparagraph">
+            <h3>Ceci est un petit titre, pour une sous partie</h3>
+            <div>Ceci est un bout de texte.</div>
+            <div>Pour chaque retour à la ligne, faire un div séparé.</div>
+          </div>
+          <div className="tc_Tac_subparagraph">
+            <h3>Une autre sous partie</h3>
+            <div>Avec du texte</div>
+          </div>
+        </div>
+
+        <div className="tc_Tac_paragraph">
+          <h2>Une autre grande partie</h2>
+          <div>Le texte direct, sans sous-partie</div>
+          <div>
+            Si ya besoin d'une liste :
+            <ul>
+              <li>Premier point</li>
+              <li>Deuxième point</li>
+            </ul>
+            Ca fait une liste.
+          </div>
+          <div>
+            Si ya besoin d'une liste avec des numéros :
+            <ol>
+              <li>Premier point</li>
+              <li>Deuxième point</li>
+            </ol>
+            Ca fait une liste avec des numéros.
+          </div>
+        </div>
 			</div>
 		);
 	}
