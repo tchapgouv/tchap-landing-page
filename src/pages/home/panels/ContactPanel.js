90037d7ad2d2c6a5aa68dbbf43024b375f8aed4a
JDixmier <45665538+JDixmier@users.noreply.github.com>
2022-02-07 18:09:01 +0100

    Clarification du texte et ajustement de la mise en forme
    <div Tchap ne fonctionne pas normalement ? > 
    <div Vous avez besoin d'aide pour vous en servir ?>
    <div Le support est joignable à l'adresse <b>support@tchap.beta.gouv.fr</b>.>
    <div Pour toute sollicitation plus générale, vous pouvez contacter l'équipe à <b>{t('links.contact')}</b>.>

--- src/pages/home/panels/ContactPanel.js
+++ src/pages/home/panels/ContactPanel.js
@@ -11,8 +11,10 @@ class ContactPanel extends Component {
 						<div class="fr-callout fr-mb-4w">
 							<p class="fr-callout__title">Contactez-nous</p>
 							<p class="fr-callout__text">
-								Le support de Tchap est joignable à l'adresse <b>support@tchap.beta.gouv.fr</b>. 
-							  	Pour toute autre question, vous pouvez nous envoyer un email à <b>{t('links.contact')}</b>.
+								<div Tchap ne fonctionne pas normalement ? > 
+									<div Vous avez besoin d'aide pour vous en servir ?>
+									<div Le support est joignable à l'adresse <b>support@tchap.beta.gouv.fr</b>.>
+							  	<div Pour toute sollicitation plus générale, vous pouvez contacter l'équipe à <b>{t('links.contact')}</b>.>
 							</p>
 							<a class="fr-btn"
 							   href={'mailto:' + t('links.contact')}
