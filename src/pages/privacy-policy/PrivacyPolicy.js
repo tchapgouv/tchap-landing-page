236a4c131d8c0fd97367bd1394c3295b82b2b793
Juliette <juliette.dixmier@sciencespo.fr>
2022-05-31 17:50:19 +0200

    Amélioration mise en page - Privacy Policy

--- src/pages/privacy-policy/PrivacyPolicy.js
+++ src/pages/privacy-policy/PrivacyPolicy.js
@@ -31,9 +31,11 @@ class PrivacyPolicy extends Component {
 							<div>
 								Ne sont traitées que les données strictement nécessaires au fonctionnement de l’Application. Ces données ne pourront en aucun cas servir au contrôle et à la surveillance de l’activité des Utilisateurs et Utilisatrices.
 							</div>
+							<br/>
 							<div>
 								Les données à caractère personnel concernées sont les suivantes :
 							</div>
+							<br/>
 							<div>
 								<ul>
 									<li><span className="tc_text_b">Données relatives au profil :</span> prénom, nom, organisation et adresse mail professionnelle (obligatoire), une photographie d’identité (optionnel) ;</li>
@@ -55,17 +57,21 @@ class PrivacyPolicy extends Component {
 						<div className="tc_Tac_subparagraph">
 							<h3>3.1 Données relatives au profil</h3>
 							<div>Le traitement est nécessaire à l’exécution d’une mission d’intérêt public ou relevant de l’exercice de l’autorité publique dont est investi le responsable de traitement au sens de l’article 6-e du règlement (UE) 2016/679 du Parlement européen et du Conseil du 27 avril 2016 relatif à la protection des personnes physiques à l’égard du traitement des données à caractère personnel et à la libre circulation de ces données.</div>
+							<br/>
 							<div>La mission d’intérêt public constitue la mise à disposition et le développement d’outils collaboratifs par la DINUM en vue de leur intégration dans les ministères <a href="https://www.legifrance.gouv.fr/loda/id/JORFTEXT000039281619/2019-10-28#_blank">(article 6 du décret n°2019-1088 du 25 octobre 2019 relatif au système d’information et de communication de l’Etat et à la direction interministérielle du numérique).</a></div>
+							<br/>
 							<div>Les données de profil permettent également de réaliser des statistiques anonymisées sur l’usage de la plateforme.</div>
 						</div>
 						<div className="tc_Tac_subparagraph">
 							<h3>3.2 Données de contenus du service de messagerie instantanée</h3>
 							<div>Le traitement est nécessaire à l’exécution d’une mission d’intérêt public ou relevant de l’exercice de l’autorité publique dont est investi le responsable de traitement au sens de l’article 6-e du règlement (UE) 2016/679 du Parlement européen et du Conseil du 27 avril 2016 relatif à la protection des personnes physiques à l’égard du traitement des données à caractère personnel et à la libre circulation de ces données.</div>
+							<br/>
 							<div>La mission d’intérêt public constitue la mise à disposition et le développement d’outils collaboratifs par la DINUM en vue de leur intégration dans les ministères <a href="https://www.legifrance.gouv.fr/loda/id/JORFTEXT000039281619/2019-10-28#_blank">(article 6 du décret n°2019-1088 du 25 octobre 2019 relatif au système d’information et de communication de l’Etat et à la direction interministérielle du numérique).</a></div>
 						</div>
 						<div className="tc_Tac_subparagraph">
 							<h3>3.3 Données de connexion </h3>
 							<div>Ce traitement est nécessaire au respect d'une obligation légale à laquelle le responsable de traitement est soumis au sens de l'article 6-c du Règlement (UE) 2016/679 du Parlement européen et du Conseil du 27 avril 2016 relatif à la protection des personnes physiques à l'égard du traitement des données à caractère personnel et à la libre circulation de ces données.</div>
+							<br/>
 							<div>L'obligation légale est posée par la loi LCEN n° 2004-575 du 21 juin 2004 pour la confiance dans l'économie numérique et par l’article 5 du décret n° 2021-1362 du 20 octobre 2021.</div>
 						</div>
 					</div>
@@ -111,6 +117,7 @@ class PrivacyPolicy extends Component {
 							</tr>
 						</table>
 					</div>
+					<br/>
 					<div className="tc_Tac_paragraph">
 						<h2>5. Destinataires</h2>
 						<div className="tc_Tac_subparagraph">
@@ -128,6 +135,7 @@ class PrivacyPolicy extends Component {
 						<h2>6. Sous-traitants</h2>
 						<div className="tc_Tac_subparagraph">
 							<div>Certaines données sont envoyées à des sous-traitants. Le responsable de traitement s’est assuré de la mise en œuvre par ses sous-traitants de garanties adéquates et du respect de conditions strictes de confidentialité, d’usage et de protection des données.</div>
+							<br/>
 							<table class="full-width">
 								<tr>
 									<th>Partenaire</th>
@@ -166,7 +174,9 @@ class PrivacyPolicy extends Component {
 						<h2>7. Accès à la liste des contacts locaux du téléphone</h2>
 						<div className="tc_Tac_subparagraph">
 							<div>Lors de son utilisation sur un téléphone, Tchap vous demande l’autorisation d’accéder à la liste de vos contacts locaux. Il n’y a pas d’obligation à autoriser cet accès, l’Application fonctionne sans cette autorisation.</div>
+							<br/>
 							<div>Lorsque vous autorisez cet accès, Tchap l’utilise de deux manières :</div>
+							<br/>
 							<ul>
 								<li>Tchap parcourt les adresses emails de vos contacts pour découvrir d’autres Utilisateurs et Utilisatrices inscrits sur Tchap. Ces Utilisateurs et Utilisatrices sont listés dans la section ‘Contacts’ de l’Application. Ils vous sont proposés également lorsque vous souhaitez inviter des nouveaux membres à un salon. Aucune adresse email n’est stockée sur les serveurs ;</li>
 								<li>Lorsque vous souhaitez inviter des nouvelles personnes à rejoindre Tchap, les coordonnées de vos contacts locaux vous sont proposées.</li>
@@ -177,7 +187,9 @@ class PrivacyPolicy extends Component {
 						<h2>8. Cookies</h2>
 						<div className="tc_Tac_subparagraph">
 							<div>Le site dépose des cookies de mesure d’audience (nombre de visites, pages consultées), respectant les conditions d’exemption du consentement de l’internaute définies par la recommandation « Cookies » de la Commission nationale informatique et libertés (CNIL). Cela signifie, notamment, que ces cookies ne servent qu’à la production de statistiques anonymes et ne permettent pas de suivre la navigation de l’internaute sur d’autres sites.</div>
+							<br/>
 							<div>Nous utilisons pour cela Matomo, un outil de mesure d’audience web libre, paramétré pour être en conformité avec la recommandation « Cookies » de la CNIL. Cela signifie que votre adresse IP, par exemple, est anonymisée avant d’être enregistrée. Il est donc impossible d’associer vos visites sur ce site à votre personne.</div>
+							<br/>
 							<div>Il convient d’indiquer que :</div>
 							<ul>
 								<li>Les données collectées ne sont pas recoupées avec d’autres traitements.</li>
@@ -192,6 +204,8 @@ class PrivacyPolicy extends Component {
 								 data-probe-name="cookies">
 								Modifier les réglages
 						</a>
+						<br/>
+						<br/>
 						<div>Pour aller plus loin, vous pouvez consulter les fiches proposées par la Commission Nationale de l'Informatique et des Libertés (CNIL) :</div>
 							<ul>
 								<li><a href="https://www.cnil.fr/fr/cookies-traceurs-que-dit-la-loi">Cookies & traceurs : que dit la loi ?</a></li>
@@ -204,19 +218,22 @@ class PrivacyPolicy extends Component {
 						<h2>9. Sécurité</h2>
 						<div className="tc_Tac_subparagraph">
 							<div>Pour des raisons de sécurité, les contenus privés sont chiffrés et inaccessibles à un quelconque tiers. Toutes les interactions réalisées sur l’Application sont protégées. Ainsi, aucun travail de modération par la DINUM n’est possible et la DINUM ne traitera aucune demande de suppression de contenu.</div>
+							<br/>
 							<div>Le chiffrement de bout en bout consiste à chiffrer et déchiffrer les messages sur les appareils des Utilisateurs et Utilisatrices. Lorsque vous envoyez un message à quelqu'un, il est chiffré avant de sortir de votre appareil. Par ailleurs vos traces d’activité (« métadonnées ») sur des réseaux non sécurisés sont protégées par un chiffrement du canal de transmission entre l’application et le serveur Tchap (TLS 1.2).</div>
+							<br/>
 							<div>Un contrôle des documents échangés est effectué avant leur remise aux destinataires et le document n’est pas remis tant que le contenu n’est pas vérifié. Il donne lieu à un déchiffrement du document dans une zone technique isolée, une vérification du type de fichier et des signatures virales, immédiatement suivie d’une destruction sécurisée de sa copie. Il ne constitue pas un engagement de la DINUM sur son innocuité.</div>
 						</div>
 					</div>
 					<div className="tc_Tac_paragraph">
 						<h2>10. Exercice des droits à la protection des données à caractère personnel </h2>
 						<div className="tc_Tac_subparagraph">
-							<div>Vous disposez d’un droit d’accès et de modification des données à caractère personnel qui vous concernent. Vous pouvez également vous opposer aux traitements réalisés par l’Application. Ces droits s’exercent auprès de la DINUMà l’adresse tchap@beta.gouv.fr</div>
+							<div>Vous disposez d’un droit d’accès et de modification des données à caractère personnel qui vous concernent. Vous pouvez également vous opposer aux traitements réalisés par l’Application. Ces droits s’exercent auprès de la DINUM à l’adresse tchap@beta.gouv.fr</div><br/>
 							<div>Pour exercer vos droits, vous pouvez également contacter le délégué à la protection des données (DPD) des services du Premier Ministre :</div>
 							<ul>
 								<li>par mail à dpd@pm.gouv.fr</li>
 								<li>
-									ou par courrier à l’adresse suivante :
+									ou par courrier à l’adresse suivante :<br/>
+									<br/>
 									<div>
 										<div>Services du Premier Ministre</div>
 										<div>À l’attention du délégué à la protection des données (DPD)</div>
