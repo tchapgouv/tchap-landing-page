6ffbad5a4dfeb9824ad94b89bf67ab1efc6ba2a0
Angelo Chiacchio <angelo@areox.net>
2023-12-11 17:25:54 +0100

    Updated users stats number

--- src/pages/home/panels/WelcomePanel.js
+++ src/pages/home/panels/WelcomePanel.js
@@ -15,7 +15,7 @@ class WelcomePanel extends Component {
 							<Grid item md={7} xs={12} className="tc_WelcomePanel_block">
 								<h1>Tchap, la messagerie instantanée du Secteur Public</h1>
 								<div className="fr-text--lead">Conçue et gérée par l'Administration française, pour les agents des trois fonctions publiques, pour communiquer facilement en toute sécurité.</div>
-								<div className="fr-text--lead">Utilisée par plus de 430 000 agents publics.</div>
+								<div className="fr-text--lead">Utilisée par plus de 460 000 agents publics.</div>
 								<div className="tc_WelcomePanel_content">
 									<WelcomePanelBtns />
 								</div>
